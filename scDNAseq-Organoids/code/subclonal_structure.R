4a0da961cd9bd58fd34017c9b4f125091ff1f4f8
Lena Morrill <lm687@cam.ac.uk>
2021-03-25 13:29:19 +0000

    modified dendrogram fig 2

--- scDNAseq-Organoids/code/subclonal_structure.R
+++ scDNAseq-Organoids/code/subclonal_structure.R
@@ -6,6 +6,8 @@ library(ggdendro)
 library(pheatmap)
 library(readxl)
 library(gridExtra)
+library(ggplotify)
+library(cowplot)
 
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
@@ -29,12 +31,22 @@ organoid_list = renaming$PDO[match(gsub("orgb", "org", organoid_list), renaming$
 
 ## remove the outliers
 outliers = list()
-outliers$`118976org` = c(12, 10, 11, 7, 5, 6, 4, 3, 1, 2, 8, 9, 158)
-outliers$`119148orgb` = c(13, 1, 2, 8, 12, 9, 3, 10, 7, 6, 5, 4, 11, 379, 381)
-outliers$`23868org` = c(1,2,3, 4, 5, 6, 7, 8)
+outliers$`PDO3` = c(12, 10, 11, 7, 5, 6, 4, 3, 1, 2, 8, 9, 158)
+outliers$`PDO6` = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 23, 377, 378, 379, 380, 381)
+outliers$`PDO2` = c(1,2,3, 4, 5, 6, 7, 8)
 
-names(outliers) = renaming$PDO[match(gsub("orgb", "org", names(outliers)), renaming$ID)]
+# plot(hclust(dist(absCN[[org_it]][-outliers[[org_it]],])))
 
+clean_chrom = function(i){
+  sapply(i, function(j){
+    if(j %in% c('X', 'Y')){
+      j
+    }else{
+      ## autosomal
+      substr(j, 2, 1000)
+    }
+  })
+}
 
 for(org_it in organoid_list){
   absCN[[org_it]] = absCN[[org_it]][-outliers[[org_it]],]
@@ -49,9 +61,13 @@ for(org_it in organoid_list){
   
   mat_breaks <- c(-Inf, 0:6, seq(8, 14, by=2)) #seq(min(org_clean, na.rm = T), max(org_clean, na.rm = T), length.out = 10)
   col_list <- c("#2670af", "#8daecf", "#eaeaea", "#ffd2b6", "#f5b788", "#f39d5f", "#f17e37", "#ee1200", "#b60000", "#7a0e04", "#401004", "#000000")
+  annotation_chroms = data.frame(row.names = colnames(absCN[[org_it]]),
+                                 chrom=clean_chrom(gsub("\\..*", "", colnames(absCN[[org_it]]))))
+  
   ph = pheatmap::pheatmap(absCN[[org_it]], show_colnames = FALSE, show_rownames = FALSE,
                      color             = col_list,
-                     breaks            = mat_breaks, cluster_cols = FALSE)
+                     breaks            = mat_breaks, cluster_cols = FALSE,
+                     annotation_col = annotation_chroms, annotation_legend=F, main=org_it)
   ph
   saveRDS(list(mat_breaks=mat_breaks,col_list=col_list), paste0("../robjects/fig2_colours.RDS"))
   saveRDS(absCN[[org_it]], paste0("../robjects/fig2_subclonal_hclust", org_it, "_2.RDS"))
@@ -62,21 +78,30 @@ for(org_it in organoid_list){
   plot(as.dendrogram(ph$tree_row))
   pdf(paste0("../plots/subclonal_hclust", org_it, ".pdf"))
   # quartz()
-  print(grid.arrange(ggdendrogram(as.dendrogram(ph$tree_row), no.margin = TRUE)+
-                 theme(axis.line=element_blank(),axis.text.x=element_blank(),
-                       axis.text.y=element_blank(),axis.ticks=element_blank(),
-                       # axis.title.x=element_blank(),
-                       axis.title.y=element_blank(),legend.position="none")+scale_x_continuous(expand = c(0,0)),
-               ggplot(melt(t(absCN[[org_it]])), aes(y=Var1, x=factor(Var2, levels=ph$tree_row$order), fill=value))+geom_tile()+
-    # scale_colour_steps(breaks=mat_breaks,value=col_list)
-      scale_fill_gradientn(colours=col_list, breaks=mat_breaks)+
-    theme(axis.line=element_blank(),axis.text.x=element_blank(),
-          axis.text.y=element_blank(),axis.ticks=element_blank(),
-          axis.title.x=element_blank(),
-          axis.title.y=element_blank(),legend.position="none",
-          panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
-          panel.grid.minor=element_blank(),plot.background=element_blank()), nrow=2, top=org_it))
-  dev.off()
+  # print(grid.arrange(ggdendrogram(as.dendrogram(ph$tree_row), no.margin = TRUE)+
+  #                theme(axis.line=element_blank(),axis.text.x=element_blank(),
+  #                      axis.text.y=element_blank(),axis.ticks=element_blank(),
+  #                      # axis.title.x=element_blank(),
+  #                      axis.title.y=element_blank(),legend.position="none")+scale_x_continuous(expand = c(0,0)),
+  #              ggplot(melt(t(absCN[[org_it]])), aes(y=Var1, x=factor(Var2, levels=ph$tree_row$order), fill=value))+geom_tile()+
+  #   # scale_colour_steps(breaks=mat_breaks,value=col_list)
+  #     scale_fill_gradientn(colours=col_list, breaks=mat_breaks)+
+  #   theme(axis.line=element_blank(),axis.text.x=element_blank(),
+  #         axis.text.y=element_blank(),axis.ticks=element_blank(),
+  #         axis.title.x=element_blank(),
+  #         axis.title.y=element_blank(),legend.position="none",
+  #         panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
+  #         panel.grid.minor=element_blank(),plot.background=element_blank()), nrow=2, top=org_it))
+  
+  ph
+  # dendro <- ggdendrogram(as.dendrogram(ph$tree_row), no.margin = TRUE)+
+  #                  theme(axis.line=element_blank(),axis.text.x=element_blank(),
+  #                        axis.text.y=element_blank(),axis.ticks=element_blank(),
+  #                        # axis.title.x=element_blank(),
+  #                        axis.title.y=element_blank(),legend.position="none")+scale_x_continuous(expand = c(0,0))+
+  #   coord_flip()
+  # cowplot::plot_grid(ggplotify::as.grob(ph), dendro)
+  # dev.off()
   
 }
 
