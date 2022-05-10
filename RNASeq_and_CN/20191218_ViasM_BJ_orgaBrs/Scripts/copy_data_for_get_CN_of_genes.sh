217fe78810552f531f23799aa8b79e7a75e8fdfd
lm687 <morril01@clust1-headnode-1.cri.camres.org>
2022-05-10 16:11:47 +0100

    RNASeq_DE_resistant_sensitive/files/

---
+++ RNASeq_and_CN/20191218_ViasM_BJ_orgaBrs/Scripts/copy_data_for_get_CN_of_genes.sh
@@ -0,0 +1,4 @@
+rsync -a /Users/morril01/Documents/PhD/other_repos/Vias_Brenton/copy_number_analysis_organoids/data/clean_segtables morril01@clust1-headnode.cri.camres.org:/Users/morril01/links_to_projects/Vias_git/Organoids_Compositional_Analysis/copy_number_analysis_organoids/data/
+
+
+
